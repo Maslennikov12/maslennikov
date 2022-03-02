7984a95e59fb79d720fa0c2f86121c491e1e47ed
Maslennikov12 <Maslennikov12@gmail.com>
2022-03-02 11:48:10 +0500

    Programs

---
+++ Circle/Circle/Krug.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Krug
+    {
+        //задаем поля класса
+        private double radius;
+        private double x;
+        private double y;
+        private double x0;
+        private double y0;
+
+        //метод для доступа к полю "radius"
+        public double Radius
+        {
+        get  { return radius; }
+            set {
+                if ((double)value < radius)
+                    radius = value;
+                
+            }
+            
+        }
+
+        // метод для доступа к полю "koordinate"
+        public double X
+        {
+            //возвращает значение
+            get { return x; }
+            //устанавливает значение
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            //возвращает значение
+            get { return y; }
+            //устанавливает значение
+            set { y = value; }
+        }
+
+        public double Y0
+        {
+            //возвращает значение
+            get { return y0; }
+            //устанавливает значение
+            set { y0 = value; }
+        }
+
+        public double X0
+        {
+            //возвращает значение
+            get { return x0; }
+            //устанавливает значение
+            set { x0 = value; }
+        }
+        public  Krug ()
+        {
+            radius = 50;
+            x = 20;
+            y = 10;
+        }
+
+        public Krug(double radius, double x, double y, double x0, double y0)
+        {
+            this.radius = radius;
+            this.x = x;
+            this.y = y;
+            this.x0 = x0;
+            this.y0 = y0;
+        }
+
+        //метод подсчета площади окружности
+        public double S (double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        //метод подсчета длины окружности
+        public double Line( double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        //метод для нахождения положения точки
+        public bool prin(double radius, double x, double y, double y0, double x0)
+        {
+            if (Math.Pow(x - x0, 2) +  Math.Pow(y - y0, 2) <= Math.Pow(radius, 2))           
+                return true;
+                else
+                    return false;
+            }
+        }
+
+    }
+
---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double x0, y0, x, y;
+            Krug newkrug = new Krug();
+            Console.WriteLine("Введите радиус: ");
+            newkrug.Radius = Convert.ToInt32(Console.ReadLine());
+            // проверка на ноль
+            if (newkrug.Radius == 0)
+            {
+                Console.WriteLine("Радиус не может быть равен нулю");
+                Console.ReadKey();
+            }
+            // проверка на ввод отрицательного радиуса
+            else if (newkrug.Radius < 0)
+            {
+                Console.WriteLine("Радиус не может быть отрицательным");
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.WriteLine("x0 = ");
+                newkrug.X0 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("y0 = ");
+                newkrug.Y0 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("x = ");
+                newkrug.X = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("y = ");
+                newkrug.Y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Площадь окружности = " + newkrug.S(newkrug.Radius)); //  вывод площади
+                Console.WriteLine("Длина окружности = " + newkrug.Line(newkrug.Radius)); // вывод длины окружности
+                if (newkrug.prin(newkrug.X, newkrug.Y, newkrug.X0, newkrug.Y0, newkrug.Radius))
+                    Console.WriteLine("Точка принадлежит окружности");
+                else
+                    Console.WriteLine("Точка не принадлежит окружности");               
+                Console.ReadKey();
+            }
+        }
+    }
+}
